59f7a11a2f59344a5b3498defead136a3285b9ee
ezickyle <47272290+ezickyle@users.noreply.github.com>
2020-04-21 23:40:24 +0800

    Updated april21

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,7 +6,7 @@ const Home = () => (
   <div className='Hand'>
   <h1 className="App-title">Welcome To My Site.</h1>
   <div>
-   <p className='NOOB'>The best part of being alive is to enjoy every second of it. Just leave all your worries to me, your Virtual partner.</p>
+   <p className='NOOB'><body className="niss">The best part of being alive is to enjoy every second of it. Just leave all your worries to me, your Virtual partner.</body></p>
    </div>
   </div>
    <div className='Jor'>
