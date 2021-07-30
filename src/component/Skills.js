34e3d05bb18ec0d08a4f1a09cb5c6aaa9ba36c07
ezickyle <47272290+ezickyle@users.noreply.github.com>
2021-07-30 16:07:53 +0800

    Update 7/30

--- src/component/Skills.js
+++ src/component/Skills.js
@@ -6,25 +6,27 @@ const Skills = () => (
   <h1 className="Mind">Skills</h1>
   <div>
     <div className="yat">
-    <header className="boom2">THIS IS THE ANIMATION I MADE USING 3DS MAX. IT WAS A SIMULATION PARKING OF OUR VAN. I CAN DO ANIMATION AT THE SAMETIME I CAN MAP STUFFS.</header>
+    <header className="boom2">THIS IS THE ANIMATION I MADE USING 3DS MAX ALSO SOME VIDEO OF MY YOUTUBE CHANNEL. THE ANIMATION WAS A SIMULATION PARKING OF OUR VAN. I CAN DO ANIMATION AND VIDEO EDITING ALL AT OUNCE.</header>
     <header className="route"></header>
-
+        
+        <p className="monday">This video is about Halloween Last year, showing a gameplay of alcatraz and shooting skill in Call Of Duty Mobile. </p>
         <iframe className= "uno" width="560" height="315" src="https://www.youtube.com/embed/mKxwXuvDCYQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <p className="monday">helllo there blablablablabalbalabalbalaba</p>
         
+        <p className="tuesday">Here is another video of my channel it mostly about how I looted too much ammos and weapon during this game. This was made during the looting season in the US.</p>
         <iframe className="tres" width="560" height="315" src="https://www.youtube.com/embed/ghbDREnPTG8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <p className="tuesday">helllo there blablablablabalbalabalbalaba</p>
-        
+       
+        <p className="wednesday">Here is simulation of our van going outwards.</p>
         <iframe className='singko' width="560" height="315" src="https://www.youtube.com/embed/tYzXazUvCYQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="happy"></iframe>
-        <p className="wednesday">helllo there blablablablabalbalabalbalaba</p>
-
+        
+        <p className="thursday">Here is a simulation of our van going inside the guarage, and parking.</p>
         <iframe className="dos" width="560" height="315" src="https://www.youtube.com/embed/dR6Tjfglvjo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="seem"></iframe>
-        <p className="thursday">helllo there blablablablabalbalabalbalaba</p>
+        
 
     </div>
-
+    <img className="Notting" src='./Copyrights trans.ico' alt='icon'/>
    </div> 
    
+  
   </div>
   
 );
