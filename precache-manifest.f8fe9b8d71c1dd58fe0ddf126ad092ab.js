d6460636ba4181d8cb621b4041ecea8d3a54be1b
ezickyle <ezekyle.vila2018@gmail.com>
2021-10-15 08:18:10 +0800

    Updates

---
+++ precache-manifest.f8fe9b8d71c1dd58fe0ddf126ad092ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aea7f6394fbd6dc3e36",
+    "url": "/Webtest/static/css/main.5cd03055.chunk.css"
+  },
+  {
+    "revision": "1aea7f6394fbd6dc3e36",
+    "url": "/Webtest/static/js/main.1aea7f63.chunk.js"
+  },
+  {
+    "revision": "2d1d99a500f71d4b464b",
+    "url": "/Webtest/static/js/1.2d1d99a5.chunk.js"
+  },
+  {
+    "revision": "360111b3d094e68af528",
+    "url": "/Webtest/static/js/runtime~main.360111b3.js"
+  },
+  {
+    "revision": "6bd36b7f856a34fc6353b8a95e60ce42",
+    "url": "/Webtest/static/media/Laptop201.6bd36b7f.jpg"
+  },
+  {
+    "revision": "3d6895c5592da42275aa44f931aa952d",
+    "url": "/Webtest/static/media/businesscard.3d6895c5.jpeg"
+  },
+  {
+    "revision": "9329d78d6cdace34cec89306b496d635",
+    "url": "/Webtest/static/media/social.9329d78d.jpg"
+  },
+  {
+    "revision": "f7da144c05fb4d12113c7841de23eb3c",
+    "url": "/Webtest/index.html"
+  }
+];
