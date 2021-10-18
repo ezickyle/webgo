bd78ca1d1b607185bf90b912d76b440034e1449b
ezickyle <ezekyle.vila2018@gmail.com>
2021-10-18 11:54:25 +0800

    new

---
+++ build/precache-manifest.ca95ff65076e41250ef64ef4a0ed2217.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e67bf818db0d7a855d5e",
+    "url": "/webgo/static/css/main.394e9388.chunk.css"
+  },
+  {
+    "revision": "e67bf818db0d7a855d5e",
+    "url": "/webgo/static/js/main.e67bf818.chunk.js"
+  },
+  {
+    "revision": "2d1d99a500f71d4b464b",
+    "url": "/webgo/static/js/1.2d1d99a5.chunk.js"
+  },
+  {
+    "revision": "1427c8381ddfa00eacb1",
+    "url": "/webgo/static/js/runtime~main.1427c838.js"
+  },
+  {
+    "revision": "6bd36b7f856a34fc6353b8a95e60ce42",
+    "url": "/webgo/static/media/Laptop201.6bd36b7f.jpg"
+  },
+  {
+    "revision": "3d6895c5592da42275aa44f931aa952d",
+    "url": "/webgo/static/media/businesscard.3d6895c5.jpeg"
+  },
+  {
+    "revision": "9329d78d6cdace34cec89306b496d635",
+    "url": "/webgo/static/media/social.9329d78d.jpg"
+  },
+  {
+    "revision": "5d0f6de8ae3f536a7de7a6612b503df7",
+    "url": "/webgo/index.html"
+  }
+];
